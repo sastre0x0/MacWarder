6424ae95d941fb035ea180712150dce278993911
sastre2 <axeld844@gmail.com>
2023-02-20 11:38:23 -0600

    update

--- MacWarder.py
+++ MacWarder.py
@@ -11,10 +11,17 @@ parser.add_argument("-m", "--mac", dest="new_mac", help="MAC address you want to
 
 args = parser.parse_args()
 
+def change_mac():
+    print("[+] " + inter + " " + "MAC address will be changed for" + " " + new_mac)
+    subprocess.call(["ifconfig", inter, "down"])
+    subprocess.call(["ifconfig", inter, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", inter, "up"])
+    subprocess.call(["ifconfig", inter])
 
 if args.inter and args.new_mac:
     inter = args.inter
     new_mac = args.new_mac
+
 else:
     subprocess.call(["ifconfig"])
     inter = input("interface > ")
@@ -24,9 +31,5 @@ if new_mac.lower() == "r" or new_mac.lower() == "random":
     new_mac = ":".join([str(hex(random.randint(0x00, 0xff)))[2:].zfill(2) for _ in range(6)])
     print("Randomly generated MAC address:", new_mac)
 
-print("[+] " + inter + " " + "MAC address will be changed for" + " " + new_mac)
-subprocess.call(["ifconfig", inter, "down"])
-subprocess.call(["ifconfig", inter, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", inter, "up"])
-subprocess.call(["ifconfig", inter])
-print("Done, thanks for your preference :)")
+change_mac()
+
